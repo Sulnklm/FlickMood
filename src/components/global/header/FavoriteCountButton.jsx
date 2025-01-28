16f7fb8981f424bb46bd2b7bbf9854dbc00b3366
Sulnklm <hyeinsuin@icloud.com>
2025-01-28 03:20:44 -0800

    header FavoriteCountButton size update

--- src/components/global/header/FavoriteCountButton.jsx
+++ src/components/global/header/FavoriteCountButton.jsx
@@ -32,10 +32,10 @@ const FavoriteCountButton = () => {
   return (
     <li className="flex items-center justify-center gap-1.5">
       <Link to="/favorite">
-        <FontAwesomeIcon icon={faHeart} className="text-xl lg:text-2xl text-white" />
+        <FontAwesomeIcon icon={faHeart} className="text-2xl text-white" />
       </Link>
       {favoriteCount > 0 && (
-        <span className="bg-customMint font-[500] text-sm rounded-full px-1.5">
+        <span className="bg-customMint font-[600] text-xs rounded-full px-[5px]">
           {favoriteCount}
         </span>
       )}
--- src/components/global/header/Header.jsx
+++ src/components/global/header/Header.jsx
@@ -6,7 +6,7 @@ import FavoriteCountButton from "./FavoriteCountButton";
 
 function Header() {
   return (
-    <header className="sticky bg-customGreen top-0 shadow-md px-5 z-[1000]">
+    <header className="sticky bg-customGreen top-0 shadow-md px-3.5 z-[1000]">
       <div className="container mx-auto flex items-center gap-3 py-3">
         <Link to="/" className="flex justify-start">
           <h2 className="font-[500] text-xl text-customMint">FlickMood</h2>
