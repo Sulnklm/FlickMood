c2e0f543c4e7df82bcca58a0fbb83ee76ed80075
Sulnklm <hyeinsuin@icloud.com>
2025-01-28 03:12:29 -0800

    home sidebar section margin update

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -5,9 +5,9 @@ import Trailers from "./sidebar/Trailers";
 
 function Sidebar() {
   return (
-    <div className="mx-5 mt-5 xl:space-y-5">
+    <div className="mx-3 xl:mx-5 mt-3 xl:mt-5 space-y-3 xl:space-y-5">
       <Trailers />
-      <div className="grid mt-5 xl:mt-0 md:flex gap-5 flex-grow xl:grid xl:space-y-5">
+      <div className="grid mt-5 xl:mt-0 md:flex gap-3 xl:gap-5 flex-grow xl:grid">
         <ForYou />
         <RecentlyViewed />
       </div>
